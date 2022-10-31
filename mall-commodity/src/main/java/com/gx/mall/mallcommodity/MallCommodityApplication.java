502828079b5e5dcb990be113ccdcd0fcef930048
guoxinliyanru1 <2548146983@qq.com>
2022-10-31 11:03:51 +0800

    temp

---
+++ mall-commodity/src/main/java/com/gx/mall/mallcommodity/MallCommodityApplication.java
@@ -0,0 +1,13 @@
+package com.gx.mall.mallcommodity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallCommodityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallCommodityApplication.class, args);
+    }
+
+}
